96f8c3ce94efa4c12060ead0a630777f6fdad28f
suin <suin00h@korea.ac.kr>
2025-05-21 10:36:24 +0000

    git init

---
+++ models/discriminator.py
@@ -0,0 +1,12 @@
+from torch import nn
+
+
+class Discriminator(nn.module):
+    """
+    'PatchGAN' classifier described in the original pix2pix paper.
+        It can classify whether 70×70 overlapping patches are real or fake.
+        Such a patch-level discriminator architecture has fewer parameters
+        than a full-image discriminator and can work on arbitrarily-sized images
+        in a fully convolutional fashion.
+    """
+    pass
